736013461c77664be8c101d3607ff09b835c019b
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-10-24 21:38:20 +0200

    Updated pipeline

--- src/main/kotlin/org/wagham/db/pipelines/characters/CharactersWithPlayer.kt
+++ src/main/kotlin/org/wagham/db/pipelines/characters/CharactersWithPlayer.kt
@@ -1,11 +1,14 @@
 package org.wagham.db.pipelines.characters
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.mongodb.client.model.UnwindOptions
 import org.bson.Document
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.conversions.Bson
 import org.litote.kmongo.lookup
 import org.litote.kmongo.match
+import org.litote.kmongo.project
+import org.litote.kmongo.unwind
 import org.wagham.db.enums.CharacterStatus
 import org.wagham.db.models.Building
 import org.wagham.db.models.Errata
@@ -14,7 +17,7 @@ import java.util.*
 
 data class CharacterWithPlayer (
     @BsonId val name: String,
-    val player: List<Player> = listOf(),
+    val player: Player,
     val race: String?,
     val territory: String?,
     @JsonProperty("class") val characterClass: String?,
@@ -34,13 +37,21 @@ data class CharacterWithPlayer (
     val proficiencies: List<String> = listOf()
 )
 
+
 class CharactersWithPlayer {
     companion object {
 
         fun getPipeline(status: CharacterStatus?): List<Bson> {
             val matchStep = if (status != null) listOf(match(Document(mapOf("status" to status))))
                 else listOf()
-            return matchStep + lookup("players", "player", "_id", "player")
+            return matchStep +
+                    lookup("players", "player", "_id", "player") +
+                    unwind("\$player", UnwindOptions()
+                        .includeArrayIndex("unwindCounter")
+                        .preserveNullAndEmptyArrays(false)) +
+                    match(Document(mapOf("unwindCounter" to 0))) +
+                    project(Document(mapOf("unwindCounter" to 0)))
+
         }
 
     }
