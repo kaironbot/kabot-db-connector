0d22721ab1d651f80707b4d4ee5d239eda15a4ca
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-01-28 19:00:02 +0100

    Added functions to add and remove proficiencies

--- src/main/kotlin/org/wagham/db/scopes/characters.kt
+++ src/main/kotlin/org/wagham/db/scopes/characters.kt
@@ -1,14 +1,12 @@
 package org.wagham.db.scopes
 
-import com.mongodb.client.model.FindOneAndUpdateOptions
-import com.mongodb.client.model.ReturnDocument
+import com.mongodb.reactivestreams.client.ClientSession
 import org.bson.Document
-import org.litote.kmongo.addToSet
-import org.litote.kmongo.eq
+import org.litote.kmongo.*
 import org.wagham.db.KabotMultiDBClient
 import org.wagham.db.enums.CharacterStatus
-import org.wagham.db.exceptions.InvalidGuildException
 import org.wagham.db.exceptions.NoActiveCharacterException
+import org.wagham.db.exceptions.ResourceNotFoundException
 import org.wagham.db.models.Character
 import org.wagham.db.pipelines.characters.CharacterWithPlayer
 
@@ -18,38 +16,75 @@ class KabotDBCharacterScope(
 ) {
 
     suspend fun getActiveCharacter(guildId: String, playerId: String): Character {
-        return client.getGuildDb(guildId)?.let {
+        return client.getGuildDb(guildId).let {
             val col = it.getCollection<Character>("characters")
             col.findOne(Character::status eq CharacterStatus.active, Character::player eq playerId)
                 ?: throw NoActiveCharacterException(playerId)
-        } ?: throw InvalidGuildException(guildId)
+        }
     }
 
+    suspend fun getCharacter(guildId: String, characterName: String): Character =
+        client.getGuildDb(guildId)
+            .getCollection<Character>("characters")
+            .findOne(Character::name eq characterName)
+            ?: throw ResourceNotFoundException(characterName, "characters")
+
+    suspend fun getCharacter(session: ClientSession, guildId: String, characterName: String): Character =
+        client.getGuildDb(guildId)
+            .getCollection<Character>("characters")
+            .findOne(session, Character::name eq characterName)
+            ?: throw ResourceNotFoundException(characterName, "characters")
+
+
     fun getAllCharacters(guildId: String, status: CharacterStatus? = null) =
-        client.getGuildDb(guildId)?.getCollection<Character>("characters")
-            ?.find(Document(
+        client.getGuildDb(guildId).getCollection<Character>("characters")
+            .find(Document(
                 status?.let {
                     mapOf("status" to status)
                 } ?: emptyMap<String, String>()
             ))
-            ?.toFlow()
-            ?: throw InvalidGuildException(guildId)
+            .toFlow()
 
-    suspend fun addCharacterProficiency(guildId: String, character: String, proficiency: String) =
-        client.getGuildDb(guildId)?.let {
+    suspend fun addCharacterProficiency(guildId: String, characterName: String, proficiency: String) =
+        client.getGuildDb(guildId)
+            .getCollection<Character>("characters")
+            .updateOne(
+                Character::name eq characterName,
+                addToSet(Character::proficiencies, proficiency)
+            ).modifiedCount == 1L
+
+    suspend fun addCharacterProficiency(session: ClientSession, guildId: String, characterName: String, proficiency: String) =
+        client.getGuildDb(guildId)
+            .getCollection<Character>("characters")
+            .updateOne(
+                session,
+                Character::name eq characterName,
+                addToSet(Character::proficiencies, proficiency)
+            ).modifiedCount == 1L
+
+    suspend fun removeCharacterProficiency(guildId: String, characterName: String, proficiency: String) =
+        client.getGuildDb(guildId)
+            .getCollection<Character>("characters")
+            .updateOne(
+                Character::name eq characterName,
+                pull(Character::proficiencies, proficiency),
+            ).modifiedCount == 1L
+
+    suspend fun subtractMoney(session: ClientSession, guildId: String, characterName: String, qty: Float) =
+        client.getGuildDb(guildId).let {
+            val character = getCharacter(session, guildId, characterName)
             it.getCollection<Character>("characters")
-                .findOneAndUpdate(
-                    Character::name eq character,
-                    addToSet(Character::proficiencies, proficiency),
-                    FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER)
-                )?.proficiencies?.contains(proficiency) ?: false
-        } ?: throw InvalidGuildException(guildId)
+                .updateOne(
+                    session,
+                    Character::name eq characterName,
+                    setValue(Character::money, character.money - qty),
+                ).modifiedCount == 1L
+        }
 
     fun getCharactersWithPlayer(guildId: String, status: CharacterStatus? = null) =
         client.getGuildDb(guildId)
-            ?.getCollection<Character>("characters")
-            ?.aggregate<CharacterWithPlayer>(CharacterWithPlayer.getPipeline(status))
-            ?.toFlow()
-            ?: throw InvalidGuildException(guildId)
+            .getCollection<Character>("characters")
+            .aggregate<CharacterWithPlayer>(CharacterWithPlayer.getPipeline(status))
+            .toFlow()
 
 }
