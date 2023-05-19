e5e529a68661a698b3421f4f886be72704bd7774
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-05-19 23:28:26 +0200

    Small fix

--- src/main/kotlin/org/wagham/db/utils/ResultUtils.kt
+++ src/main/kotlin/org/wagham/db/utils/ResultUtils.kt
@@ -1,10 +1,11 @@
 package org.wagham.db.utils
 
 import com.mongodb.client.result.UpdateResult
+import kotlin.math.min
 
 fun UpdateResult.isSuccessful() =
     (this.modifiedCount == 1L).takeIf {
         this.upsertedId == null
     } ?: true
 
-fun <T> List<T>.subList(start: Int?, end: Int?): List<T> = subList(start ?: 0, end ?: size)
+fun <T> List<T>.subList(start: Int?, end: Int?): List<T> = subList(start ?: 0, min(end ?: size, size))
