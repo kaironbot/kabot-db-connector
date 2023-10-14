a1dd7207dc69083a8b77a48c6be9886579148039
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-10-14 21:32:44 +0200

    Added time required to ability cost

--- src/main/kotlin/org/wagham/db/models/embed/AbilityCost.kt
+++ src/main/kotlin/org/wagham/db/models/embed/AbilityCost.kt
@@ -2,5 +2,6 @@ package org.wagham.db.models.embed
 
 data class AbilityCost(
     val moCost: Float = 0f,
-    val itemsCost:  Map<String, Int> = emptyMap()
+    val itemsCost:  Map<String, Int> = emptyMap(),
+    val timeRequired: Long? = 0
 )
