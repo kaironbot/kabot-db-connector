f26d6fdfbbb2fbbfaf9f39a03371730a75cf7fd2
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-12-02 17:35:59 +0100

    :bug: Removed BsonId from label stub (#92)

--- src/main/kotlin/org/wagham/db/models/embed/LabelStub.kt
+++ src/main/kotlin/org/wagham/db/models/embed/LabelStub.kt
@@ -1,10 +1,9 @@
 package org.wagham.db.models.embed
 
 import kotlinx.serialization.Serializable
-import org.bson.codecs.pojo.annotations.BsonId
 
 @Serializable
 data class LabelStub(
-    @BsonId val id: String,
+    val id: String,
     val name: String
 )
