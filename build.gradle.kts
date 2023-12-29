4da2f876c56724fd4924f01c7b1244bde9fcbad5
Vincenzo Pierro <vcp@icure.com>
2023-12-29 16:09:14 +0100

    :arrow_up: bumped dependencies and kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.20"
     id("io.kotest") version "0.3.8"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
     id("org.sonarqube") version "3.3"
     id("maven-publish")
     id("jacoco")
@@ -30,22 +30,27 @@ repositories {
 }
 
 dependencies {
-    implementation("org.litote.kmongo:kmongo-coroutine:4.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.kotest:kotest-runner-junit5-jvm:5.5.3")
-    implementation("org.slf4j:slf4j-api:2.0.3")
-    implementation("org.slf4j:slf4j-simple:2.0.3")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.3")
-    testImplementation("io.kotest:kotest-framework-engine-jvm:5.5.3")
+    implementation("org.litote.kmongo:kmongo-coroutine:4.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
+    implementation("org.slf4j:slf4j-api:2.0.5")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
+    testImplementation("io.kotest:kotest-framework-engine-jvm:5.5.5")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_19
+    targetCompatibility = JavaVersion.VERSION_19
+}
+
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "19"
 }
 
 publishing {
