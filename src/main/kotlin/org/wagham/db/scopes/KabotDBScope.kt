3990ad302fc65a521ba84d4b11f25cc4f916c4eb
Vincenzo Pierro <vcp@icure.com>
2024-01-03 11:50:58 +0100

    :memo: Updated Doc on interface

--- src/main/kotlin/org/wagham/db/scopes/KabotDBScope.kt
+++ src/main/kotlin/org/wagham/db/scopes/KabotDBScope.kt
@@ -8,6 +8,12 @@ interface KabotDBScope<T : Any> {
     val client: KabotMultiDBClient
     val collectionName: String
 
+    /**
+     * Returns the [CoroutineCollection] associated to the type [T] of the scope.
+     *
+     * @param guildId the id of the guild from which to take the collection.
+     * @return a [CoroutineCollection] of [T].
+     */
     fun getMainCollection(guildId: String): CoroutineCollection<T>
 
 }
