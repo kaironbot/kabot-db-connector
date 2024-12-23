ca6a2f1ecdc0be13f8af11b747c8301a30f4a755
Vincenzo Pierro <vcp@icure.com>
2024-12-23 11:39:37 +0100

    Fixed missing dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.20"
-    id("io.kotest") version "0.3.8"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
-    id("jacoco")
+	kotlin("jvm") version "1.9.20"
+	id("io.kotest") version "0.3.8"
+	id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
+	id("jacoco")
 }
 
 buildscript {
-    repositories {
-        mavenCentral()
-    }
+	repositories {
+		mavenCentral()
+	}
 }
 
 
@@ -18,29 +18,30 @@ group = "org.wagham"
 version = "0.21.0"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.litote.kmongo:kmongo-coroutine:4.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
-    implementation("org.slf4j:slf4j-api:2.0.5")
-    implementation("org.slf4j:slf4j-simple:2.0.5")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
-    testImplementation("io.kotest:kotest-framework-engine-jvm:5.5.5")
+	implementation("org.litote.kmongo:kmongo-coroutine:4.11.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+	runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.1")
+	implementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
+	implementation("org.slf4j:slf4j-api:2.0.5")
+	implementation("org.slf4j:slf4j-simple:2.0.5")
+	testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
+	testImplementation("io.kotest:kotest-framework-engine-jvm:5.5.5")
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_19
-    targetCompatibility = JavaVersion.VERSION_19
+	sourceCompatibility = JavaVersion.VERSION_19
+	targetCompatibility = JavaVersion.VERSION_19
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "19"
+	kotlinOptions.jvmTarget = "19"
 }
