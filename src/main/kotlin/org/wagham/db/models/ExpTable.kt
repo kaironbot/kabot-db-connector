bd9bc28b57f1ce49655fc7c1d16e97eac76694a0
Vincenzo Pierro <vcp@icure.com>
2024-12-28 10:35:17 +0100

    ExpTable is now serializable

--- src/main/kotlin/org/wagham/db/models/ExpTable.kt
+++ src/main/kotlin/org/wagham/db/models/ExpTable.kt
@@ -1,25 +1,27 @@
 package org.wagham.db.models
 
+import kotlinx.serialization.Serializable
 import org.bson.codecs.pojo.annotations.BsonId
 import org.wagham.db.utils.findPrevious
 import java.util.*
 
+@Serializable
 data class ExpTable (
-    @BsonId val utilType: String,
-    val table: SortedMap<Int, String>,
-    val tier: Map<Int, String> = emptyMap()
+	@BsonId val utilType: String,
+	val table: SortedMap<Int, String>,
+	val tier: Map<Int, String> = emptyMap()
 ) {
 
-    private val levelToExp = table.keys.fold(mapOf<String, Int>()) { acc, it ->
-        acc + (table[it]!! to it)
-    }
-    private val tierToExp = tier.keys.fold(mapOf<String, Int>()) { acc, it ->
-        acc + (tier[it]!! to it)
-    }
-    fun levelToExp(level: String) = levelToExp[level] ?: throw IllegalArgumentException("This level does not exist")
-    fun tierToExp(tier: String) = tierToExp[tier] ?: throw IllegalArgumentException("This tier does not exist")
-    fun expToLevel(exp: Float) = table[table.keys.toList().findPrevious(exp)]
-        ?: throw IllegalArgumentException("No level for provided exp")
-    fun expToTier(exp: Float) = tier[tier.keys.toList().findPrevious(exp)]
-    ?: throw IllegalArgumentException("No tier for provided exp")
+	private val levelToExp = table.keys.fold(mapOf<String, Int>()) { acc, it ->
+		acc + (table[it]!! to it)
+	}
+	private val tierToExp = tier.keys.fold(mapOf<String, Int>()) { acc, it ->
+		acc + (tier[it]!! to it)
+	}
+	fun levelToExp(level: String) = levelToExp[level] ?: throw IllegalArgumentException("This level does not exist")
+	fun tierToExp(tier: String) = tierToExp[tier] ?: throw IllegalArgumentException("This tier does not exist")
+	fun expToLevel(exp: Float) = table[table.keys.toList().findPrevious(exp)]
+		?: throw IllegalArgumentException("No level for provided exp")
+	fun expToTier(exp: Float) = tier[tier.keys.toList().findPrevious(exp)]
+		?: throw IllegalArgumentException("No tier for provided exp")
 }
--- src/main/kotlin/org/wagham/db/scopes/KabotDBUtilityScope.kt
+++ src/main/kotlin/org/wagham/db/scopes/KabotDBUtilityScope.kt
@@ -12,93 +12,93 @@ import org.wagham.db.utils.isSuccessful
 import org.wagham.db.utils.limit
 
 class KabotDBUtilityScope(
-    private val client: KabotMultiDBClient
+	private val client: KabotMultiDBClient
 ) {
 
-    suspend fun getExpTable(guildId: String) =
-        client.getGuildDb(guildId)
-           .getCollection<ExpTable>(CollectionNames.UTILS)
-           .findOne( ExpTable::utilType eq "msTable")
-            ?: throw ResourceNotFoundException("ExpTable", "utils")
+	suspend fun getExpTable(guildId: String) =
+		client.getGuildDb(guildId)
+			.getCollection<ExpTable>(CollectionNames.UTILS)
+			.findOne( ExpTable::utilType eq "msTable")
+			?: throw ResourceNotFoundException("ExpTable", "utils")
 
-    suspend fun getPlayableResources(guildId: String) =
-        client.getGuildDb(guildId)
-            .getCollection<PlayableResources>(CollectionNames.UTILS)
-            .findOne( PlayableResources::docId eq "playableResources")
-            ?: throw ResourceNotFoundException("Playable Resources", "utils")
+	suspend fun getPlayableResources(guildId: String) =
+		client.getGuildDb(guildId)
+			.getCollection<PlayableResources>(CollectionNames.UTILS)
+			.findOne( PlayableResources::docId eq "playableResources")
+			?: throw ResourceNotFoundException("Playable Resources", "utils")
 
-    suspend fun getAnnouncements(guildId: String, batchId: String) =
-        client.getGuildDb(guildId)
-            .getCollection<AnnouncementBatch>(CollectionNames.ANNOUNCEMENTS)
-            .findOne(AnnouncementBatch::id eq batchId)
-            ?: throw ResourceNotFoundException("Announcements", "announcements")
+	suspend fun getAnnouncements(guildId: String, batchId: String) =
+		client.getGuildDb(guildId)
+			.getCollection<AnnouncementBatch>(CollectionNames.ANNOUNCEMENTS)
+			.findOne(AnnouncementBatch::id eq batchId)
+			?: throw ResourceNotFoundException("Announcements", "announcements")
 
-    suspend fun updateAnnouncements(guildId: String, batchId: String, batch: AnnouncementBatch) =
-        client.getGuildDb(guildId)
-            .getCollection<AnnouncementBatch>(CollectionNames.ANNOUNCEMENTS)
-            .updateOne(
-                AnnouncementBatch::id eq batchId,
-                batch,
-                UpdateOptions().upsert(true)
-            ).isSuccessful()
+	suspend fun updateAnnouncements(guildId: String, batchId: String, batch: AnnouncementBatch) =
+		client.getGuildDb(guildId)
+			.getCollection<AnnouncementBatch>(CollectionNames.ANNOUNCEMENTS)
+			.updateOne(
+				AnnouncementBatch::id eq batchId,
+				batch,
+				UpdateOptions().upsert(true)
+			).isSuccessful()
 
-    fun getBuildingsMessages(guildId: String) =
-        client.getGuildDb(guildId)
-            .getCollection<PlayerBuildingsMessages>(CollectionNames.BUILDING_MESSAGES)
-            .find()
-            .toFlow()
+	fun getBuildingsMessages(guildId: String) =
+		client.getGuildDb(guildId)
+			.getCollection<PlayerBuildingsMessages>(CollectionNames.BUILDING_MESSAGES)
+			.find()
+			.toFlow()
 
-    suspend fun updateBuildingMessage(guildId: String, message: PlayerBuildingsMessages) =
-        client.getGuildDb(guildId)
-            .getCollection<PlayerBuildingsMessages>(CollectionNames.BUILDING_MESSAGES)
-            .updateOne(
-                PlayerBuildingsMessages::id eq message.id,
-                message,
-                UpdateOptions().upsert(true)
-            ).isSuccessful()
+	suspend fun updateBuildingMessage(guildId: String, message: PlayerBuildingsMessages) =
+		client.getGuildDb(guildId)
+			.getCollection<PlayerBuildingsMessages>(CollectionNames.BUILDING_MESSAGES)
+			.updateOne(
+				PlayerBuildingsMessages::id eq message.id,
+				message,
+				UpdateOptions().upsert(true)
+			).isSuccessful()
 
-    suspend fun updateAttendance(guildId: String, attendanceReport: AttendanceReport) =
-        client.getGuildDb(guildId)
-            .getCollection<AttendanceReport>(CollectionNames.ATTENDANCE)
-            .updateOne(
-                AttendanceReport::date eq attendanceReport.date,
-                attendanceReport,
-                UpdateOptions().upsert(true)
-            ).isSuccessful()
+	suspend fun updateAttendance(guildId: String, attendanceReport: AttendanceReport) =
+		client.getGuildDb(guildId)
+			.getCollection<AttendanceReport>(CollectionNames.ATTENDANCE)
+			.updateOne(
+				AttendanceReport::date eq attendanceReport.date,
+				attendanceReport,
+				UpdateOptions().upsert(true)
+			).isSuccessful()
 
-    suspend fun getLastAttendance(guildId: String) =
-        client.getGuildDb(guildId)
-            .getCollection<AttendanceReport>(CollectionNames.ATTENDANCE)
-            .find()
-            .descendingSort(AttendanceReport::date)
-            .first()
+	suspend fun getLastAttendance(guildId: String) =
+		client.getGuildDb(guildId)
+			.getCollection<AttendanceReport>(CollectionNames.ATTENDANCE)
+			.find()
+			.descendingSort(AttendanceReport::date)
+			.first()
 
-    suspend fun getLastMarket(guildId: String): WeeklyMarket? =
-        client.getGuildDb(guildId)
-            .getCollection<WeeklyMarket>(CollectionNames.MARKETS)
-            .find()
-            .descendingSort(WeeklyMarket::date)
-            .first()
+	suspend fun getLastMarket(guildId: String): WeeklyMarket? =
+		client.getGuildDb(guildId)
+			.getCollection<WeeklyMarket>(CollectionNames.MARKETS)
+			.find()
+			.descendingSort(WeeklyMarket::date)
+			.first()
 
-    /**
-     * @param guildId the id of the guild.
-     * @param limit the number of elements to retrieve. Null will retrieve all the elements.
-     * @return a [Flow] of [WeeklyMarket] sorted descending by [WeeklyMarket.date].
-     */
-    fun getLastMarkets(guildId: String, limit: Int? = null): Flow<WeeklyMarket> =
-        client.getGuildDb(guildId)
-            .getCollection<WeeklyMarket>(CollectionNames.MARKETS)
-            .find()
-            .limit(limit)
-            .descendingSort(WeeklyMarket::date)
-            .toFlow()
+	/**
+	 * @param guildId the id of the guild.
+	 * @param limit the number of elements to retrieve. Null will retrieve all the elements.
+	 * @return a [Flow] of [WeeklyMarket] sorted descending by [WeeklyMarket.date].
+	 */
+	fun getLastMarkets(guildId: String, limit: Int? = null): Flow<WeeklyMarket> =
+		client.getGuildDb(guildId)
+			.getCollection<WeeklyMarket>(CollectionNames.MARKETS)
+			.find()
+			.limit(limit)
+			.descendingSort(WeeklyMarket::date)
+			.toFlow()
 
-    suspend fun updateMarket(guildId: String, market: WeeklyMarket) =
-        client.getGuildDb(guildId)
-            .getCollection<WeeklyMarket>(CollectionNames.MARKETS)
-            .updateOne(
-                WeeklyMarket::date eq market.date,
-                market,
-                UpdateOptions().upsert(true)
-            ).isSuccessful()
+	suspend fun updateMarket(guildId: String, market: WeeklyMarket) =
+		client.getGuildDb(guildId)
+			.getCollection<WeeklyMarket>(CollectionNames.MARKETS)
+			.updateOne(
+				WeeklyMarket::date eq market.date,
+				market,
+				UpdateOptions().upsert(true)
+			).isSuccessful()
 }
